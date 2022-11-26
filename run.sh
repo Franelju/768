8700ba652d6f091de31bd3be761ddbcb48b19180
Franelju <frane.sazunic@gmail.com>
2022-11-25 21:36:17 -0500

    slurm fix

--- run.sh
+++ run.sh
@@ -3,7 +3,13 @@
 #SBATCH -p general
 #SBATCH -N 1
 #SBATCH --mem=5g
-#SBATCH -t 15:00:00 --mail-type=end --mail-user=daftari@email.unc.edu --wrap="python3 run_paths_pool.py”
+#SBATCH -t 15:00:00 
+#SBATCH --mail-type=end 
+#SBATCH --mail-user=frane@email.unc.edu 
+#SBATCH--wrap="python3 run_paths_pool.py”
+#SBATCH -o ./report/output.%a.out # STDOUT
 
 module add python/3.7.9
+
+python3 run_paths_pool.py
 
