2582408eb00f016169482281279e775876726a12
Franelju <frane.sazunic@gmail.com>
2022-11-26 14:38:25 -0500

    re test 24

--- run.sh
+++ run.sh
@@ -2,11 +2,12 @@
 
 #SBATCH -p general
 #SBATCH -N 1
+#SBATCH -n 12
 #SBATCH --mem=5g
 #SBATCH -t 15:00:00 
 #SBATCH --mail-type=end 
 #SBATCH --mail-user=frane@email.unc.edu 
-#SBATCH -o ./report/output.%a.out # STDOUT
+#SBATCH -o ~/report/output.%a.out # STDOUT
 
 module add python/3.7.9
 pip install timebudget
